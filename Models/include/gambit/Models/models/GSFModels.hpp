681c3a68a2432dfe867ce3d51d80d7592955dd02
Fabio Zeiser <fzeiser@users.noreply.github.com>
2020-10-16 15:55:23 +0200

    Added another NLD (BSFG) model + initialization via Gledeli (#1)
    * Added a function "set_model_names" in the GAMBIT frontend for gledeli. Use it to pass a list with names of active GAMBIT models from NuclearBit to gledeli.
    * Updated submodule, including minor fixes
    
    Co-authored-by: Anders Kvellestad <anders.kvellestad@fys.uio.no>

--- Models/include/gambit/Models/models/GSFModels.hpp
+++ Models/include/gambit/Models/models/GSFModels.hpp
@@ -2,13 +2,13 @@
 //   *********************************************
 ///  \file
 ///
-///  GAMBIT models for nuclear physics 
+///  GAMBIT models for nuclear physics
 ///  gamma-ray strength functions
 ///
 ///  *********************************************
 ///
 ///  Authors (add name and date if you modify):
-///   
+///
 ///  \author Anders Kvellestad
 ///          anders.kvellestad@fys.uio.no
 ///  \date 2020 Mar
@@ -18,31 +18,6 @@
 
 #ifndef __gsfmodels_hpp__
 #define __gsfmodels_hpp__
-
-#define MODEL GSFModel2
-  START_MODEL
-  DEFINEPARS(gsf_p1,gsf_p2)
-#undef MODEL
-
-#define MODEL GSFModel3
-  START_MODEL
-  DEFINEPARS(gsf_p1,gsf_p2,gsf_p3)
-#undef MODEL
-
-#define MODEL GSFModel4
-  START_MODEL
-  DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4)
-#undef MODEL
-
-#define MODEL GSFModel5
-  START_MODEL
-  DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5)
-#undef MODEL
-
-#define MODEL GSFModel10
-  START_MODEL
-  DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5,gsf_p6,gsf_p7,gsf_p8,gsf_p9,gsf_p10)
-#undef MODEL
 
 #define MODEL GSFModel20
   START_MODEL
