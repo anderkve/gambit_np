b49d0a6343ffcebc245fb21bbcb29a3f03734973
fzeiser <fzeiser@physnet.uni-hamburg.de>
2020-10-16 15:40:46 +0200

    Finished adding models

--- Models/include/gambit/Models/models/NLDModels.hpp
+++ Models/include/gambit/Models/models/NLDModels.hpp
@@ -24,19 +24,9 @@
   DEFINEPARS(nld_T, nld_Eshift, nld_Ecrit)
 #undef MODEL
 
-#define MODEL NLDModel10
+#define MODEL NLDModelBSFG_and_discretes
   START_MODEL
-  DEFINEPARS(nld_p1,nld_p2,nld_p3,nld_p4,nld_p5,nld_p6,nld_p7,nld_p8,nld_p9,nld_p10)
-#undef MODEL
-
-#define MODEL NLDModel15
-  START_MODEL
-  DEFINEPARS(nld_p1,nld_p2,nld_p3,nld_p4,nld_p5,nld_p6,nld_p7,nld_p8,nld_p9,nld_p10,nld_p11,nld_p12,nld_p13,nld_p14,nld_p15)
-#undef MODEL
-
-#define MODEL NLDModel20
-  START_MODEL
-  DEFINEPARS(nld_p1,nld_p2,nld_p3,nld_p4,nld_p5,nld_p6,nld_p7,nld_p8,nld_p9,nld_p10,nld_p11,nld_p12,nld_p13,nld_p14,nld_p15,nld_p16,nld_p17,nld_p18,nld_p19,nld_p20)
+  DEFINEPARS(nld_NLDa, nld_Eshift, nld_Ecrit)
 #undef MODEL
 
 #endif /* defined(__nldmodels_hpp__) */
