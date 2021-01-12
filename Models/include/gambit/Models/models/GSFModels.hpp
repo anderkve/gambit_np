241a93f35b38c015b9a076ce7d9d6faaee25025a
fzeiser <fzeiser@physnet.uni-hamburg.de>
2021-01-12 15:07:40 +0100

    Add (slightly hacky) constantM1 gsf & modify names
    - added somewhat hacky version to get a constant M1 strength function, if one defines the additional model GSF_ConstantM1
    - modified the names of the GSF Models slightly to emphasise when/that the temperature of the final states is set constant.

--- Models/include/gambit/Models/models/GSFModels.hpp
+++ Models/include/gambit/Models/models/GSFModels.hpp
@@ -24,14 +24,29 @@
   DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5,gsf_p6,gsf_p7,gsf_p8,gsf_p9,gsf_p10,gsf_p11,gsf_p12,gsf_p13,gsf_p14,gsf_p15,gsf_p16,gsf_p17,gsf_p18,gsf_p19,gsf_T)
 #undef MODEL
 
-#define MODEL GSF_GLOModel20
+#define MODEL GSF_GLO_CT_Model20
   START_MODEL
   DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5,gsf_p6,gsf_p7,gsf_p8,gsf_p9,gsf_p10,gsf_p11,gsf_p12,gsf_p13,gsf_p14,gsf_p15,gsf_p16,gsf_p17,gsf_p18,gsf_p19,gsf_T)
 #undef MODEL
 
-#define MODEL GSF_EGLOModel20
+#define MODEL GSF_EGLO_CT_Model20
   START_MODEL
   DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5,gsf_p6,gsf_p7,gsf_p8,gsf_p9,gsf_p10,gsf_p11,gsf_p12,gsf_p13,gsf_p14,gsf_p15,gsf_p16,gsf_p17,gsf_p18,gsf_p19,gsf_T,gsf_epsilon_0,gsf_k)
+#undef MODEL
+
+#define MODEL GSF_MGLO_CT_Model20
+  START_MODEL
+  DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5,gsf_p6,gsf_p7,gsf_p8,gsf_p9,gsf_p10,gsf_p11,gsf_p12,gsf_p13,gsf_p14,gsf_p15,gsf_p16,gsf_p17,gsf_p18,gsf_p19,gsf_T,gsf_epsilon_0,gsf_k)
+#undef MODEL
+
+#define MODEL GSF_GH_CT_Model20
+  START_MODEL
+  DEFINEPARS(gsf_p1,gsf_p2,gsf_p3,gsf_p4,gsf_p5,gsf_p6,gsf_p7,gsf_p8,gsf_p9,gsf_p10,gsf_p11,gsf_p12,gsf_p13,gsf_p14,gsf_p15,gsf_p16,gsf_p17,gsf_p18,gsf_p19,gsf_T,gsf_epsilon_0,gsf_k)
+#undef MODEL
+
+#define MODEL GSF_constantM1
+  START_MODEL
+  DEFINEPARS(gsf_constantM1)
 #undef MODEL
 
 #endif /* defined(__gsfmodels_hpp__) */
